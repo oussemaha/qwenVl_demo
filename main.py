6c06943224397ad6bb2024ed89d15b558f924371
oussemah <oussemahamouda6@gmail.com>
2025-02-03 22:09:10 +0100

    few fixes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
+import torch
 
 import gradio as gr
-from .constants import *
+from constants import *
+
+import json
 
 
 model = Qwen2VLForConditionalGeneration.from_pretrained(
@@ -13,7 +16,7 @@ model = Qwen2VLForConditionalGeneration.from_pretrained(
 )
 processor = AutoProcessor.from_pretrained(MODEL_NAME)
 
-def read_file_to_string(file_path):
+def read_file_to_string(file_path:str):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
@@ -56,13 +59,21 @@ def extract_data_QWEN(file):
         return_tensors="pt",
     )
     inputs = inputs.to("cuda")
-    generated_ids = model.generate(**inputs, max_new_tokens=128)
+    generated_ids = model.generate(**inputs, max_new_tokens=256)
     generated_ids_trimmed = [
         out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
     ]
     result = processor.batch_decode(
         generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
     )
+    print(type(result))
+    result=result[0]        
+    result.replace('\n','')
+    result.replace('\t','')
+
+    result=result[result.find('{'):result.find('}')+1]
+    result=json.loads(result)
+    print(result)
     return result
 
 def greet(name,file):
@@ -70,9 +81,11 @@ def greet(name,file):
     return "Hello, " + name + "!"
 
 demo = gr.Interface(
-    fn=extract_data_QWEN,
-    inputs=["file"],
-    outputs=["text"],
+    fn=extract_data_QWEN,  # Function to call
+    inputs=gr.Image(type="pil", label="Upload Image"),  # Input: Image (as PIL image)
+    outputs=gr.JSON(label="Output JSON"),  # Output: JSON
+    title="Invoice to JSON Processor",
+    description="Upload an image of the invoice and get the content as JSON."
 )
 
 demo.launch(share=True)
